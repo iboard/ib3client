e0b45bbc62ffbcbadaabdc6e1dd1ab347af26127
Andi Altendorfer <andi@iboard.cc>
2008-11-06 16:52:14 +0100

    Patch ib3client master with global changes from branch ispboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,20 @@ module ApplicationHelper
     end
     "<h1>#{str}</h1>"
   end
+  
+  def navigation(str)
+    content_for :navigation do
+      str
+    end
+  end
+  
+  def page_block(&block)
+    concat("<div class='page_block'>", block.binding)
+    yield
+    concat("</div>", block.binding)
+  end
 
+  def toggle_div_tag( label, tag_id )
+    "<a href=# onclick=\"Element.toggle('#{tag_id}');\">#{label}</a>"
+  end
 end

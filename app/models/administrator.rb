087eb6aa46dc4810249961917b2b5ea772b61abf
Andi Altendorfer <andi@iboard.cc>
2008-11-07 15:33:23 +0100

    Tree view of administrators improved, and using named_scopes for servers

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -3,4 +3,16 @@ class Administrator < ActiveRecord::Base
   validates_uniqueness_of :username
   has_many                :servers, :foreign_key => :user_id
   acts_as_tree
+  
+  named_scope :top_level_administrators, lambda { {:conditions => ['parent_id = 0 or parent_id = ?', nil]} }
+  
+  def fullname
+    user = User.find(:all).detect { |u| 
+      u.username == username
+    }
+    [user.given_name, user.sn].join(" ")
+  end
+  
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveResource::Base
   self.site = "http://remote_client:remote@localhost:3002"
   
+  
+  def self.not_administrators(but_me)
+    User.find(:all).find_all { |u|
+      u.username == but_me || Administrator.find_by_username(u.username).nil?
+    }
+  end
+  
   def self.find_by_username(username)
     @user ||= User.find(:first, :conditions => ['username = ?', username])
   end
